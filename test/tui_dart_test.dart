4108d2d0872e330da1b96b2a41ba714e1f38f010
ibtissam <ibtissamwannas21@gmail.com>
2025-03-23 09:22:04 +0200

    Remove all tests from tui_dart_test.dart

--- test/tui_dart_test.dart
+++ test/tui_dart_test.dart
@@ -1,16 +1,1 @@
-import 'package:tui_dart/tui_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+void main() {}
